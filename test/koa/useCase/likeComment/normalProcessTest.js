4723b2ff9afda7b8187c4a35b1bd3f31c5d0cafb
linmadan <772181827@qq.com>
2019-05-20 16:31:34 +0800

    添加评论与回复点赞接口

---
+++ test/koa/useCase/likeComment/normalProcessTest.js
@@ -0,0 +1,75 @@
+const http = require('http')
+const expect = require('chai').expect
+const request = require('supertest')
+const app = require('../../../../lib/koa')
+const {
+    createDBConnection
+} = require('../../../../lib/infrastructure')
+
+describe('对评论点赞/取消点赞用例测试', () => {
+    let server
+    let dBConnection
+    before(done => {
+        server = http.createServer(app.callback()).listen(3000, (err) => {
+            if (err) {
+                done(err)
+            } else {
+                dBConnection = createDBConnection()
+                dBConnection.connect().then(() => {
+                    done()
+                }).catch(err => {
+                    done(err)
+                })
+            }
+        })
+    })
+    after(done => {
+        server.close(err => {
+            if (err) {
+                done(err)
+            } else {
+                dBConnection.disconnect().then(() => {
+                    done()
+                }).catch(err => {
+                    done(err)
+                })
+            }
+        })
+    })
+    describe('对评论点赞/取消点赞', () => {
+        it('对评论点赞', done => {
+            request(server)
+                .post("/poetry-voices/1/comments/1/up")
+                .send({})
+                .set('Accept', 'application/json')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    if (err) {
+                        done(err)
+                        return
+                    }
+                    expect(res.body.code).to.equal(0)
+                    expect(res.body.msg).to.equal("ok")
+                    done()
+                })
+        })
+        it('对评论取消点赞', done => {
+            request(server)
+                .post("/poetry-voices/1/comments/1/down")
+                .send({})
+                .set('Accept', 'application/json')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    if (err) {
+                        done(err)
+                        return
+                    }
+                    expect(res.body.code).to.equal(0)
+                    expect(res.body.msg).to.equal("ok")
+                    done()
+                })
+        })
+    })
+})
