0ecb65c36e3cf64eec5817dbf3792177fcce577d
linmadan <772181827@qq.com>
2019-07-03 12:03:24 +0800

    添加获取声音接口

---
+++ test/koa/useCase/getPoetryVoice/normalProcessTest.js
@@ -0,0 +1,58 @@
+const http = require('http')
+const expect = require('chai').expect
+const request = require('supertest')
+const app = require('../../../../lib/koa')
+const {
+    createDBConnection
+} = require('../../../../lib/infrastructure')
+
+describe('根据声音id获取演唱声音信息用例测试', () => {
+    let server
+    let dBConnection
+    before(done => {
+        server = http.createServer(app.callback()).listen(3000, (err) => {
+            if (err) {
+                done(err)
+            }else {
+                dBConnection = createDBConnection()
+                dBConnection.connect().then(() => {
+                    done()
+                }).catch(err => {
+                    done(err)
+                })
+            }
+        })
+    })
+    after(done => {
+        server.close(err => {
+            if (err) {
+                done(err)
+            }else {
+                dBConnection.disconnect().then(() => {
+                    done()
+                }).catch(err => {
+                    done(err)
+                })
+            }
+        })
+    })
+    describe('根据声音id获取演唱声音信息', () => {
+        it('返回演唱声音信息', done => {
+            request(server)
+                .get("/poetry-voices/3197673279606016")
+                .set('Accept', 'application/json')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    if (err) {
+                        done(err)
+                        return
+                    }
+                    expect(res.body.code).to.equal(0)
+                    expect(res.body.msg).to.equal("ok")
+                    expect(res.body.data).to.exist
+                    done()
+                })
+        })
+    })
+})
